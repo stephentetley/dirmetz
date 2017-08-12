b18039c8674641fe237a3ef3dde510acb478b5ea
stephentetley <stephen.tetley@gmail.com>
2017-08-12 23:08:25 +0100

    Implemented the Walker by first implementing congruence combinators for Folder and File.

--- demo/DemoFileListing.hs
+++ demo/DemoFileListing.hs
@@ -19,11 +19,34 @@ data FileObj = File Name
   deriving (Show)
 
 
+-- Note the Lam example distributed with KURE implies we don't 
+-- need a universe as we have a single type
+
 data U = UFO FileObj
 
 instance Injection FileObj U where
   inject = UFO
   project (UFO fo) = Just fo
+
+
+-- Congruence combinator                     
+fileT :: Monad m => (Name -> b) -> Transform c m FileObj b
+fileT f = contextfreeT $ \case
+    File s -> return (f s)
+    _     -> fail "not a File"
+
+
+-- congruence combinator
+-- Note Path is not propagated (this is an error)
+folderT :: Monad m 
+        => Transform c m FileObj a -> (Name -> [a] -> b) -> Transform c m FileObj b
+folderT t f = transform $ \c -> \case
+    Folder s ks -> f s <$> mapM (\fo -> applyT t c fo) ks
+    _ -> fail "not a Folder"
+                             
+
+folderAllR :: Monad m => Rewrite c m FileObj -> Rewrite c m FileObj
+folderAllR r = folderT r Folder
 
 
 
@@ -32,12 +55,6 @@ instance Walker cx U where
   allR r = prefixFailMsg "allR failed: " $
            rewrite $ \cx -> \(UFO fo) -> inject <$> applyR allRfileObj cx fo
     where
-      allRfileObj :: Monad m => Rewrite c m FileObj
       allRfileObj = readerT $ \case 
                       File {} -> idR
-                      Folder {} -> undefined
-
---      allFileObj cx (File s) = File <$> pure s
---      allFileObj cx (Folder s fs) = undefined -- let fn = extractR r in Folder <$> pure s <*> mapM (fn cx) fs
-
-foldersAllR :: Monad m => Rewrite c m [FileObj] -> Rewrite c m [FileObj] -> Rewrite c m [FileObj]
+                      Folder {} -> folderAllR (extractR r)
