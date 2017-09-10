e8dcfee7f55b065546abb4e2cdb85ab959c907a6
stephentetley <stephen.tetley@gmail.com>
2017-09-10 10:11:53 +0100

    More work on the dir -recurse parser.

--- src/DirMetz/FileSys/DirRecurseParser.hs
+++ src/DirMetz/FileSys/DirRecurseParser.hs
@@ -36,15 +36,18 @@ type Lexer           = P.GenTokenParser String () Identity
 
 testP :: Parser a -> String -> Either String a
 testP p s = first show $ parse p "no source" s 
-      
-lineEnd :: Parser ()
-lineEnd = void endOfLine <|> eof
 
-lineOf :: Parser a -> Parser a
-lineOf p = p <* lineEnd
 
-blankLine :: Parser ()
-blankLine = spaces *> lineEnd
+
+-- Use a notational convention - Directories are not "filled" until 
+-- post-processing after parsing, hence use another name.
+-- 
+type Element = FileObj
+
+data Block = Block Name Element
+  deriving (Show)
+
+
 
 pMode :: Parser String
 pMode = many1 (lower <|> char '-')
@@ -53,7 +56,7 @@ pName :: Parser String
 pName = trimr <$> manyTill anyChar lineEnd
 
 
--- TODO can Data.Time.Format help us from being UK biased...
+-- TODO maybe Data.Time.Format can help us from being UK biased...
 -- WARNING UK Biased
 pDate :: Parser Day
 pDate = (\d m y ->  fromGregorian y m d) 
@@ -67,8 +70,67 @@ pTime = (\h m -> hoursMinutesSecondsToDiffTime h m 0)
 pUTCTime :: Parser UTCTime
 pUTCTime = UTCTime <$> lexeme pDate <*> lexeme pTime
 
+pLastWriteTime :: Parser Properties 
+pLastWriteTime = 
+    (\modtime -> Properties { access_time = Nothing
+                            , modification_time = Just modtime })
+                <$> pUTCTime
+  
+
+pSize :: Parser Size
+pSize = integer
+
+pDirectoryName :: Parser String
+pDirectoryName = spaces *> string "Directory:" *> spaces *> pName
+
+pHeadings :: Parser ()
+pHeadings = titles *> underlines
+  where
+    titles = sepStrings ["Mode", "LastWriteTime", "Length", "Name"]  
+               *> ws *> endOfLine 
+    underlines = (underline *> ws) *> (underline *> ws) *>
+                 (underline *> ws) *> (underline *> ws) *> lineEnd
+
+
+pElement :: Parser Element
+pElement = pMode >>= elementK
+  where
+    elementK mode | isDir mode  = (\props name -> Folder name props [])
+                                    <$> pLastWriteTime <*> pName
+    
+    elementK _                  = (\props size name -> File name props size)
+                                    <$> pLastWriteTime <*> pSize <*> pName
+
 --------------------------------------------------------------------------------
 -- Lexer and utils
+
+sepStrings :: [String]  -> Parser [String]
+sepStrings []           = return []
+sepStrings (s:ss)       = step s ss
+  where
+    step :: String -> [String] -> Parser [String]
+    step s1 []           = (\x -> [x]) <$> string s1
+    step s1 (c:cs)       = (:) <$> (string s1 <* spaces) <*> step c cs
+
+
+
+ws :: Parser () 
+ws = many (char ' ' <|> char '\t') *> return ()
+
+ws1 :: Parser () 
+ws1 = many1 (char ' ' <|> char '\t') *> return ()
+
+lineEnd :: Parser ()
+lineEnd = void endOfLine <|> eof
+
+lineOf :: Parser a -> Parser a
+lineOf p = p <* lineEnd
+
+blankLine :: Parser ()
+blankLine = spaces *> lineEnd
+
+underline :: Parser ()
+underline = many1 (char '-') *> return ()
 
 
 int                 :: Parser Int
@@ -90,3 +152,8 @@ trimr = foldr step []
   where
     step c [] | isSpace c = []
     step c ac             = c:ac
+
+
+isDir :: String -> Bool
+isDir ('d':_)   = True
+isDir _         = False
