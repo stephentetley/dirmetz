876c682469ae69ea590acb7761297cca1f45bda2
stephentetley <stephen.tetley@gmail.com>
2018-04-03 20:12:28 +0100

    Improvements to the Windows dir listing parser.

--- src/DirMetz/FileSys/DirRecurseParser.hs
+++ src/DirMetz/FileSys/DirRecurseParser.hs
@@ -46,24 +46,21 @@ testIOP p path = fmap (first show) $ parseFromFile p path
 
 
 
--- Use a notational convention - Directories are not "filled" until 
--- post-processing after parsing, hence use another name.
+-- Note - Directories are not "filled" until post-processing 
+-- after parsing.
 -- 
-
--- TODO should have a distinct syntax where Element/Folders are 
--- not recursive
-
-type Element = Content
 type Mode = String
+type WinFilePath = String
 
-{-
--- TODO
-data Element_ = WinFile   Mode UTCTime Size Name
-              | WinFolder Mode UTCTime Name 
+-- Use a non-recursive type so parsing matches input.
+--
+data Element = WinFolder Mode UTCTime Name
+             | WinFile   Mode UTCTime Size Name
+  deriving (Eq,Show)
 
--}
 
-data Block = Block Name [Element]
+
+data Block = Block WinFilePath [Element]
   deriving (Show)
 
 type Listing = [Block]
@@ -90,13 +87,7 @@ pTime = (\h m -> hoursMinutesSecondsToDiffTime h m 0)
 
 pUTCTime :: Parser UTCTime
 pUTCTime = UTCTime <$> lexeme pDate <*> lexeme pTime
-
-pLastWriteTime :: Parser Properties 
-pLastWriteTime = 
-    (\modtime -> Properties { access_time = Nothing
-                            , modification_time = Just modtime })
-                <$> pUTCTime
-  
+ 
 
 pSize :: Parser Size
 pSize = integer
@@ -104,7 +95,7 @@ pSize = integer
 
 -- | Note - a long name is printed on the next line.
 --
-pDirectoryName :: Parser String
+pDirectoryName :: Parser WinFilePath
 pDirectoryName = string "Directory:" *> ws1 *> rest
   where
     rest = (lineEnd *> pName) <|> pName
@@ -124,11 +115,11 @@ pHeaderLines = titles *> underlines
 pElement :: Parser Element
 pElement = pMode >>= elementK
   where
-    elementK mode | isDir mode  = (\props name -> FsFolder name props [])
-                                    <$> pLastWriteTime <*> pName
+    elementK mode | isDir mode  = (\name props -> WinFolder mode name props)
+                                    <$> pUTCTime <*> pName
     
-    elementK _                  = (\props size name -> FsFile name props size)
-                                    <$> pLastWriteTime <*> pSize <*> pName
+    elementK mode               = (\props size name -> WinFile mode props size name)
+                                    <$> pUTCTime <*> pSize <*> pName
 
 pBlock :: Parser Block
 pBlock = Block  <$> indented pDirectoryName
@@ -141,10 +132,11 @@ pListing = twice emptyLine *> many1 pBlock
         <?> "LISTING"
 
 
+
 -- Problem - output of dir -recurse seems to be in "UCS-2 LE BOM"
 -- and not UTF8, this is causing horrible errors...
 --
-readListing :: FilePath -> IO (Either String Content)
+readListing :: FilePath -> IO (Either String FileStore)
 readListing path = do 
     ans <- parseFromFile pListing path
     return $ case ans of 
@@ -152,6 +144,7 @@ readListing path = do
        Right ls -> case buildTopDown ls of
              Nothing -> Left $ "Could not build root file object"
              Just a -> Right $ a
+
 
 --------------------------------------------------------------------------------
 -- Lexer and utils
@@ -230,17 +223,13 @@ isDir _         = False
 -- Build from flat.
 
 
-buildTopDown :: Listing -> Maybe Content
+buildTopDown :: Listing -> Maybe FileStore
 buildTopDown listing = fmap build1 $ getRoot listing
-  where
-    root_props = Properties { access_time = Nothing
-                            , modification_time = Nothing }
-    
-    -- Note - Root has no properties, so we can't make with makeRecur
-    --
-    build1 (Block name es) = let level1_kids = makeLevel1Kids listing
-                                 kids = map (makeRecur level1_kids name) es
-                             in FsFolder name root_props kids
+  where    
+    build1 (Block root elts) = 
+        let level1_kids = makeLevel1Kids listing
+            kids = map (makeRecur level1_kids root) elts
+        in FileStore root kids
 
 -- | Root is always first
 getRoot :: Listing -> Maybe Block
@@ -248,16 +237,22 @@ getRoot (x:_) = Just x
 getRoot []    = Nothing
 
 
-makeRecur :: Level1Kids -> Name -> Element -> Content
-makeRecur _     _       obj@(FsFile {})               = obj
-makeRecur store parent      (FsFolder name2 props _)  = 
-   let fullname = parent ++ ('\\':name2)
+makeRecur :: Level1Kids -> String -> Element -> Content
+makeRecur _     _       (WinFile _ mtime sz name)  =
+    let props = Properties { access_time = Nothing
+                           , modification_time = Just mtime }
+    in FsFile name props sz
+
+makeRecur store pathto  (WinFolder _ mtime name)   = 
+   let fullname = pathto ++ ('\\':name)
+       props    = Properties { access_time = Nothing
+                             , modification_time = Just mtime }
        kids1    = MAP.findWithDefault [] fullname store
        kids2    = map (makeRecur store fullname) kids1
-   in FsFolder name2 props kids2
+   in FsFolder name props kids2
 
 
-type Level1Kids = MAP.Map Name [Content]
+type Level1Kids = MAP.Map WinFilePath [Element]
 
 makeLevel1Kids :: Listing -> Level1Kids
 makeLevel1Kids = foldr step MAP.empty
